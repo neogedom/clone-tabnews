d1c1be6ed3896fcc615bfee166de811438c34083
Vinícius Gomes Ferreira <45953964+neogedom@users.noreply.github.com>
2025-10-25 02:06:46 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone do TabNews para fins de aprendizado
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home () {
-    return <h1> Oi, May! Oi Bernardo! Amo vocês ♥. </h1>
+function Home() {
+  return <h1> Oi, May! Oi Bernardo! Amo vocês ♥. </h1>;
 }
 
 export default Home;
