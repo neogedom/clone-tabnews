084d1123f512d9389b4ee697e990e3ec578dd0dc
Vinícius Gomes Ferreira <45953964+neogedom@users.noreply.github.com>
2025-10-28 23:54:56 +0000

    alterando mensagemm de projeto em construção

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1> Oi, May! Oi Bernardo! Amo vocês ♥. </h1>;
+  return <>
+    <h1> Em construção... </h1>
+    <p> Aqui teremos um espaço onde eu irei tornar públicos os textos de meu projeto de Ficção Fantástica que (até o momento) ainda não saiu do papel. </p>
+    <p> Aguardem! </p>
+  </>
+  ;
 }
 
 export default Home;
