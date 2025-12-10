314ffda07be7951846b6958b51f8c44522a2e5b7
Vinícius Gomes Ferreira <45953964+neogedom@users.noreply.github.com>
2025-12-10 11:54:38 +0000

    make  more resilient using techiniques against connection leaks

--- infra/database.js
+++ infra/database.js
@@ -1,17 +1,21 @@
-import { Client } from "pg";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+});
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  try {
+    const result = await pool.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error("Database query error:", error);
+    throw error;
+  }
 }
 
 export default {
