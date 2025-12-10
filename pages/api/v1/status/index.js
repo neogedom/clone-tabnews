c56d44f749a200589052062dbf6cba222abc2d16
Vinícius Gomes Ferreira <45953964+neogedom@users.noreply.github.com>
2025-12-10 11:55:41 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
-  response.status(200).json({ chave: "acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersion = dbVersionResult.rows[0].server_version;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const usedConnectionsResult = await database.query({
+    text: "SELECT COUNT(*) AS used_connections FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const usedConnections = Number.parseInt(
+    usedConnectionsResult.rows[0].used_connections,
+  );
+
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnections = Number.parseInt(
+    maxConnectionsResult.rows[0].max_connections,
+  );
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    database_version: dbVersion,
+    used_connections: usedConnections,
+    max_connections: maxConnections,
+  });
 }
 
 export default status;
--- tests/intergration/api/v1/status/get.test.js
+++ tests/intergration/api/v1/status/get.test.js
@@ -1,4 +1,21 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  let parsedDate = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toBe(parsedDate);
+
+  expect(responseBody.database_version).toEqual("16.0");
+  expect(responseBody.database_version).not.toBeNull();
+
+  const usedConnections = responseBody.used_connections;
+  expect(usedConnections).toBeDefined();
+  expect(usedConnections).toEqual(1);
+
+  const maxConnections = responseBody.max_connections;
+  expect(maxConnections).toBeDefined();
+  expect(maxConnections).toBeGreaterThan(0);
 });
