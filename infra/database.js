7a8e4d648725fefb29089a91f587fea80b7acc8a
Vinícius Gomes Ferreira <45953964+neogedom@users.noreply.github.com>
2025-12-22 14:58:49 +0000

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -1,31 +1,31 @@
-import { Pool } from "pg";
-
-const pool = new Pool({
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  user: process.env.POSTGRES_USER,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-
-console.log({
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  user: process.env.POSTGRES_USER,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-});
+import { Client } from "pg";
 
 async function query(queryObject) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
+  });
+
+  console.log({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
   try {
     const result = await pool.query(queryObject);
     return result;
   } catch (error) {
     console.error("Database query error:", error);
     throw error;
+  } finally {
+    await client.end();
   }
 }
 
