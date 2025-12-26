c03f0987c741cadc530f564c85a3b3e428c58b96
Vinícius Gomes Ferreira <45953964+neogedom@users.noreply.github.com>
2025-12-26 13:01:15 +0000

    change ssl to be always true and log ssl value

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,8 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    // ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: true,
   });
 
   console.log({
@@ -16,6 +17,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: client.ssl,
   });
 
   try {
